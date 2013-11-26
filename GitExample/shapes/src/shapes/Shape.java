4971e19b7d3d0f5feffa269837fd06b36e8ee19f
Manoj <Manoj@Manoj-PC>
2013-11-25 20:52:42 -0800

    Version 1

---
+++ GitExample/shapes/src/shapes/Shape.java
@@ -0,0 +1,22 @@
+package shapes;
+
+public class Shape
+{
+  protected double area;
+  protected double perimeter;
+
+ public Shape()
+ {
+   area=perimeter=0;
+ }
+  public String toString()
+  	 {
+
+  	     return "area = " + area +
+                  " \t perimeter = " + perimeter;
+
+  	 }
+
+
+}
+
